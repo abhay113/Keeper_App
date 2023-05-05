a32fe68a29d624cc0848b92315ec85714aa68156
Abhay <Abhaychavan346@gmail.com>
2023-05-05 13:36:57 +0530

    completed app

---
+++ src/components/Header.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import BorderColorIcon from "@mui/icons-material/BorderColor";
+
+function Header() {
+  return (
+    <header>
+      <h1>
+        <BorderColorIcon />
+         <span></span> Keeper
+      </h1>
+    </header>
+  );
+}
+
+export default Header;
---
+++ src/notes.js
@@ -0,0 +1,29 @@
+const notes = [
+  {
+    key: 1,
+    title: "array",
+    content: "there is nothing to say in m eyes",
+  },
+  {
+    key: 2,
+    title: "rice is the reason ",
+    content: "this is the tributr to thalaiva ",
+  },
+  {
+    key: 3,
+    title: "array",
+    content: "there is nothing to say in m eyes",
+  },
+  {
+    key: 4,
+    title: "array",
+    content: "there is nothing to say in m eyes",
+  },
+  {
+    key: 5,
+    title: "array",
+    content: "there is hslnmvkasbesk iuvulhbnesikv esoilgbnes iesgjesoig esuoh to say in m eyes",
+  },
+];
+
+export default notes;
